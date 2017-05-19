18a24bd9197acdf75cdfb16736c6a9ee5dae1a3b
unghuy <ungquanghuy@gmail.com>
2017-05-19 08:50:35 +0700

    Laravel_3 file

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+
+class VerifyCsrfToken extends BaseVerifier
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+
+    public function tokensMatch($request)
+    {
+        if($request->wantsJson())
+        {
+            return true;
+        }
+        return parent::tokensMatch($request);
+    }
+}
